e29913396121ea9037bc3f41763c84ad6fdff983
Yu Mei <yu.mei@zennioptical.com>
2026-01-16 10:50:19 +0800

    test code review

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -25,12 +25,13 @@ export default async function LangLayout({
       <a
         href="#main-content"
         className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-white focus:border-2 focus:border-blue-500 focus:rounded-md"
+        style={{ display: 'none' }}
       >
         Skip to main content
       </a>
       <Header navItems={navItems} lang={lang} />
-      <main id="main-content" className="main-content-scroll-margin" style={{ display: 'none' }}>{children}</main>
-      <Footer settings={settings} lang={lang}  />
+      <main id="main-content" className="main-content-scroll-margin">{children}</main>
+      <Footer settings={settings} lang={lang} />
     </>
   )
 }
